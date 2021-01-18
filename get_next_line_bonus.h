4e3d4dbb66f259490106fa5491e6e1b4630a5ae6
hnoh <noguen@hnoh>
2021-01-18 11:46:55 +0900

    bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnoh <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/18 09:30:39 by hnoh              #+#    #+#             */
+/*   Updated: 2021/01/18 11:45:33 by nogeun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
+
+int			get_next_line(int fd, char **line);
+
+size_t		ft_strlen(const char *s);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcat(char *dst, const char *src, size_t size);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strdup(const char *s);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnoh <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/18 09:31:31 by hnoh              #+#    #+#             */
+/*   Updated: 2021/01/18 11:46:02 by nogeun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t		ft_strlen(const char *s)
+{
+	size_t		i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t		i;
+	size_t		j;
+
+	i = 0;
+	j = 0;
+	if (!src || !dst)
+		return (0);
+	while (src[j] && j < dstsize - 1 && dstsize > 0)
+	{
+		dst[j] = src[j];
+		j++;
+	}
+	if (dstsize - j > 0)
+		dst[j] = '\0';
+	while (src[i])
+		i++;
+	return (i);
+}
+
+size_t		ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t		i;
+	size_t		j;
+	size_t		destln;
+	size_t		srcln;
+
+	srcln = ft_strlen(src);
+	destln = ft_strlen(dst);
+	j = destln;
+	i = 0;
+	if (destln < size - 1 && size > 0)
+	{
+		while (src[i] && destln + i < size - 1)
+		{
+			dst[j] = src[i];
+			i++;
+			j++;
+		}
+		dst[j] = 0;
+	}
+	if (destln >= size)
+		destln = size;
+	return (destln + srcln);
+}
+
+char		*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s3;
+	int		i;
+
+	if (!s1 || !s2)
+		return (0);
+	if (!(s3 = (char*)malloc(sizeof(*s3) * (ft_strlen(s1) +
+											ft_strlen(s2) + 1))))
+		return (0);
+	i = 0;
+	while (*s1)
+		s3[i++] = *s1++;
+	while (*s2)
+		s3[i++] = *s2++;
+	s3[i] = 0;
+	return (s3);
+}
+
+char		*ft_strdup(const char *s)
+{
+	char	*str;
+	int		len;
+
+	len = ft_strlen(s) + 1;
+	if ((str = malloc(len)))
+	{
+		ft_strlcpy(str, s, len);
+		return (str);
+	}
+	return (NULL);
+}
