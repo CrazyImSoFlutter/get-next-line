709f990aaa82c98dfdcc97d137fccde374bf6924
노근(hnoh) <nogeun@hnoh.localdomain>
2021-01-18 10:53:28 +0900

    main

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,63 @@
 /*   By: hnoh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 09:28:57 by hnoh              #+#    #+#             */
-/*   Updated: 2021/01/18 10:41:46 by hnoh             ###   ########.fr       */
+/*   Updated: 2021/01/18 10:53:15 by hnoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int		is_newline(char *backup)
+{
+	int				i;
+
+	i = 0;
+	while (backup[i])
+	{
+		if (backup[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int		split(char **backup, char **line, int cut)
+{
+	char			*temp;
+	int				len;
+
+	(*backup)[cut] = '\0';
+	*line = ft_strdup(*backup);
+	len = ft_strlen(*backup + cut + 1);
+	if (len == 0)
+	{
+		free(*backup);
+		*backup = 0;
+		return (1);
+	}
+	temp = ft_strdup(*backup + cut + 1);
+	free(*backup);
+	*backup = temp;
+	return (1);
+}
+
+int		return_all(char **backup, char **line, int read_size)
+{
+	int				cut;
+
+	if (read_size < 0)
+		return (-1);
+	if (*backup && (cut = is_newline(*backup)) >= 0)
+		return (split(backup, line , cut));
+	else if (*backup)
+	{
+		*line = *backup;
+		*backup = 0;
+		return (0);
+	}
+	*line = ft_strdup("");
+	return (0);
+}
 
 int 	get_next_line(int fd, char **line)
 {
@@ -26,7 +78,7 @@ int 	get_next_line(int fd, char **line)
 		buf[read_size] = '\0';
 		backup_buf[fd] = fd_strjoin(backup[fd], buf);
 		if ((cut = is_newline(backup[fd]) >= 0))
-			return (split_line(&backup[fd], line, cut));
+			return (split(&backup[fd], line, cut));
 	}
 	return (return_all(&backup[fd], line, read_size));
 }
