c19f57ba0857a06604bb89720e8a65ab1a93146a
hnoh <noguen@hnoh>
2021-01-18 11:43:04 +0900

    final

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,15 @@
 /*   By: hnoh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 09:28:57 by hnoh              #+#    #+#             */
-/*   Updated: 2021/01/18 10:59:06 by hnoh             ###   ########.fr       */
+/*   Updated: 2021/01/18 11:32:17 by nogeun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		is_newline(char *backup)
+int			is_newline(char *backup)
 {
-	int				i;
+	int		i;
 
 	i = 0;
 	while (backup[i])
@@ -26,10 +26,10 @@ int		is_newline(char *backup)
 	return (-1);
 }
 
-int		split(char **backup, char **line, int cut)
+int			split(char **backup, char **line, int cut)
 {
-	char			*temp;
-	int				len;
+	char	*temp;
+	int		len;
 
 	(*backup)[cut] = '\0';
 	*line = ft_strdup(*backup);
@@ -46,14 +46,14 @@ int		split(char **backup, char **line, int cut)
 	return (1);
 }
 
-int		return_all(char **backup, char **line, int read_size)
+int			return_all(char **backup, char **line, int read_size)
 {
-	int				cut;
+	int		cut;
 
 	if (read_size < 0)
 		return (-1);
 	if (*backup && (cut = is_newline(*backup)) >= 0)
-		return (split(backup, line , cut));
+		return (split(backup, line, cut));
 	else if (*backup)
 	{
 		*line = *backup;
@@ -64,21 +64,21 @@ int		return_all(char **backup, char **line, int read_size)
 	return (0);
 }
 
-int 	get_next_line(int fd, char **line)
+int			get_next_line(int fd, char **line)
 {
-	static char		*backup_buf[OPEN_MAX];
-	char			buf[BUFFERSIZE + 1];
+	static char		*backup[OPEN_MAX];
+	char			buf[BUFFER_SIZE + 1];
 	int				read_size;
 	int				cut;
 
-	if ((fd < 0) || (line == 0) || (BUFFER_SIZE <= 0) )
+	if ((fd < 0) || (line == 0) || (BUFFER_SIZE <= 0))
 		return (-1);
 	while ((read_size = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[read_size] = '\0';
-		backup_buf[fd] = fd_strjoin(backup[fd], buf);
+		backup[fd] = ft_strjoin(backup[fd], buf);
 		if ((cut = is_newline(backup[fd]) >= 0))
-			return (split_line(&backup[fd], line, cut));
+			return (split(&backup[fd], line, cut));
 	}
 	return (return_all(&backup[fd], line, read_size));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hnoh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 09:30:39 by hnoh              #+#    #+#             */
-/*   Updated: 2021/01/18 09:49:44 by hnoh             ###   ########.fr       */
+/*   Updated: 2021/01/18 11:39:06 by nogeun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,16 @@
 # include <fcntl.h>
 # include <limits.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
+
+int			get_next_line(int fd, char **line);
+
+size_t		ft_strlen(const char *s);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcat(char *dst, const char *src, size_t size);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strdup(const char *s);
 
 #endif
