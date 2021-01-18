9e899e5aaec7814aefbbe14e0b08b0eef2ed8541
노근(hnoh) <nogeun@hnoh.localdomain>
2021-01-18 10:43:46 +0900

    gnlmain

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,27 @@
 /*   By: hnoh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 09:28:57 by hnoh              #+#    #+#             */
-/*   Updated: 2021/01/18 09:29:33 by hnoh             ###   ########.fr       */
+/*   Updated: 2021/01/18 10:41:46 by hnoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+int 	get_next_line(int fd, char **line)
+{
+	static char		*backup_buf[OPEN_MAX];
+	char			buf[BUFFERSIZE + 1];
+	int				read_size;
+	int				cut;
+
+	if ((fd < 0) || (line == 0) || (BUFFER_SIZE <= 0) )
+		return (-1);
+	while ((read_size = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[read_size] = '\0';
+		backup_buf[fd] = fd_strjoin(backup[fd], buf);
+		if ((cut = is_newline(backup[fd]) >= 0))
+			return (split_line(&backup[fd], line, cut));
+	}
+	return (return_all(&backup[fd], line, read_size));
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,17 @@
 /*   By: hnoh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 09:30:39 by hnoh              #+#    #+#             */
-/*   Updated: 2021/01/18 09:30:42 by hnoh             ###   ########.fr       */
+/*   Updated: 2021/01/18 09:49:44 by hnoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <limits.h>
+
+
+#endif
